3acad5e9ae70e11a189e1b75bf649aa4ef88b3f7
Abhishek <abhisheksinghasr734@gmail.com>
2023-06-06 15:39:54 +0530

    closure addi

---
+++ closureAndScope.js/exercise3.js
@@ -0,0 +1,46 @@
+/*
+    1. Create a function called "createDatabase"
+       which has no parameters
+    2. Create an array called "storage" inside
+       "createDatabase" with the following value:
+       ["Apple", "Banana", "Carrot"]
+    3. Create a function inside "createDatabase"
+       called "addToDB" that takes a single argument.
+       This function will push the argument on to
+       the "storage" array then log out the array
+    4. Create another function inside "createDatabase"
+       called "clearDB" with no parameters. This function
+       will clear all the elements out from the "storage"
+       array (erase them all)
+    5. Return an array from "createDatabase" with two values:
+       [addToDB, clearDB]
+
+    6. How would you use "createDatabase" to add values to
+       then clear out the "database" (storage array inside)?
+*/
+
+
+const createDatabase = ()=>{
+     const storage = ["Apple", "Banana", "Carrot"];
+
+     const addToDB = (item)=>{
+        storage.push(item);
+
+        console.log(storage);
+     }
+
+     const clearDB =()=>{
+        storage.length = 0;
+        console.log(storage);
+     }
+
+     return [addToDB, clearDB];
+}
+
+const [add,clear]= createDatabase();
+
+add("Coconut");
+add("Coconut");
+clear();
+add("Coconut");
+
