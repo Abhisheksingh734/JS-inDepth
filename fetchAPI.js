7273db5eba9669e7a83b63f193c50d460a045614
Abhishek <abhisheksinghasr734@gmail.com>
2023-06-09 21:03:52 +0530

    fetch

---
+++ fetchAPI.js
@@ -0,0 +1,21 @@
+// import fetch from "node-fetch";
+
+// const response = fetch("https://www.wallhaven.cc");
+// response.then((result)=>{
+//     // console.log(result);
+//     // for(const key in result){
+//     // }
+
+//     return result.text();
+// }).then((html)=>{
+//     console.log(html);
+// })
+
+
+// const fetchData = async()=>{
+//     const response = await fetch("https://www.wallhaven.cc");
+//     const html =await response.text();
+//     console.log(html);
+// }
+
+// fetchData();
